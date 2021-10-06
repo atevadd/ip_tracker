b0b541270d903daa0427a91b0ab21b150728594f
Adeniyi Tofunmi <Adeniyitofunmi@gmail.com>
2021-10-06 19:47:29 +0100

    feat: initial commit of the challenge

---
+++ README.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+- I also added a loader when the user click the search button
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- JavaScript ES6 Fetch
+- IP Geolocation API
+
+
+### Useful resources
+
+- [Leaflet](https://leafletjs.com/) 
+- [ipify](https://geo.ipify.org/) - This is an amazing documentation which helped me finally understand IP Geolocation API. I'd recommend it to anyone still learning.
+
+**Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
+
+## Author
+
+- Website - [Tofunmi Adeniyi](https://atevadd.netlify.app)
+- Frontend Mentor - [@atevadd](https://www.frontendmentor.io/profile/atevadd)
+- Twitter - [@adeniyitofunmi1](https://www.twitter.com/adeniyitofunmi1)
---
+++ js/main.js
@@ -0,0 +1,130 @@
+// ipify API Key
+const apiKey = "at_NacrplOpYQbW70gYZJtVlPStK7SKI";
+
+// The input field loader
+const loader = document.querySelector('.loader');
+
+// The error element
+const error = document.querySelector('.error');
+
+// The submit button
+const submitBtn = document.querySelector('button');
+// The search form
+const pageForm = document.querySelector('form');
+
+// LEAFLET INITIALISATION
+let mymap = L.map('map', {
+  center: [38.89511, -77.03637],
+  zoom: 13
+});
+
+
+
+// The UI class
+class UI {
+  updateUi(data) {
+    let ip = document.querySelector('.ip'),
+      loca = document.querySelector('.location'),
+      timezone = document.querySelector('.timezone'),
+      isp = document.querySelector('.isp');
+
+    ip.innerText = data.ip;
+    loca.innerText = `${data.location.city}, ${data.location.country}`;
+    timezone.innerText = `UTC ${data.location.timezone}`;
+    isp.innerText = data.isp;
+  }
+}
+
+// The search class
+class Search {
+  static find(ip) {
+    // the ipify API
+    let result = fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ip}`);
+
+    return result;
+  }
+}
+
+function initial(mymap) {
+  pageForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    loader.style.display = "block";
+    error.style.display = "none";
+
+    const search = new Search();
+    const ui = new UI();
+
+    // The input element
+    const inputedIp = document.getElementById("search").value;
+
+    Search.find(inputedIp)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        ui.updateUi(data)
+        loader.style.display = "none";
+
+        L.setView([data.location.lat, data.location.lng], 13);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+          maxZoom: 18,
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1
+        }).addTo(mymap);
+
+        L.marker([data.location.lat, data.location.lng]).addTo(mymap);
+
+      })
+      .catch(err => {
+        // error.innerText = err.message;
+        if (err.message === "Failed to fetch") {
+          error.innerText = "You're currently offline";
+          error.style.display = "inline-block";
+          loader.style.display = "none";
+        }
+
+      })
+  })
+}
+
+
+// This initiates the map on the page and gets the user ip address
+document.addEventListener('DOMContentLoaded', function () {
+  // UI class initialisation
+  const ui = new UI();
+
+  fetch('https://api.ipify.org/?format=json')
+    .then(response => {
+      return response.json();
+    })
+    .then(ip => {
+      Search.find(ip.ip)
+        .then(reponse => {
+          return reponse.json();
+        })
+        .then(data => {
+          ui.updateUi(data);
+          // const L.setView([data.location.lat, data.location.lng], 13);
+
+        })
+    })
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+  }).addTo(mymap);
+
+
+
+  // This function adds an event listener to the input field
+  initial(mymap);
+})
